6c806de8fd3c4d57590147d481c5cac978950d84
Orban Ludovic <Ludovic.Orban@terracottatech.com>
2018-04-12 08:20:45 +0000

    working prototype spawning children with win32 CreateProcessW API
    svn path=/projects/linked-child-process/branches/win32-CreateProcessW/; revision=51087

--- src/main/java/com/tc/lcp/LinkedJavaProcess.java
+++ src/main/java/com/tc/lcp/LinkedJavaProcess.java
@@ -4,6 +4,10 @@
  */
 package com.tc.lcp;
 
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.*;
+import com.sun.jna.ptr.IntByReference;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -49,6 +53,11 @@ public class LinkedJavaProcess extends Process {
   private boolean                  running;
   private boolean                  addL1Repos = true;
   private final List<StreamCopier> copiers    = Collections.synchronizedList(new ArrayList<StreamCopier>());
+
+  private InputStream processIs;
+  private OutputStream processOs;
+  private InputStream processEs;
+  private WinBase.PROCESS_INFORMATION.ByReference processInfo;
 
   public LinkedJavaProcess(String mainClassName, List<String> classArguments, List<String> jvmArgs) {
     this.mainClassName = mainClassName;
@@ -184,10 +193,134 @@ public class LinkedJavaProcess extends Process {
 
     command = fullCommandList.toArray(new String[fullCommandList.size()]);
 
-    System.err.println("Start java process with command: " + fullCommandList);
-    this.process = Runtime.getRuntime().exec(command, makeEnv(env), workingDir);
-    this.running = true;
-  }
+        System.err.println("Start java process with command: " + fullCommandList);
+        if (isWindows()) {
+            String daCmd = "";
+            for (String c : command) {
+                daCmd = daCmd + c + "  ";
+            }
+
+            WinNT.HANDLE hChildStd_IN_Rd;
+            WinNT.HANDLE hChildStd_IN_Wr;
+            WinNT.HANDLE hChildStd_OUT_Rd;
+            WinNT.HANDLE hChildStd_OUT_Wr;
+            WinNT.HANDLE hChildStd_ERR_Rd;
+            WinNT.HANDLE hChildStd_ERR_Wr;
+
+            WinNT.HANDLE[] handles = createPipe(false);
+            hChildStd_IN_Rd = handles[0];
+            hChildStd_IN_Wr = handles[1];
+
+            handles = createPipe(true);
+            hChildStd_OUT_Rd = handles[0];
+            hChildStd_OUT_Wr = handles[1];
+
+            handles = createPipe(true);
+            hChildStd_ERR_Rd = handles[0];
+            hChildStd_ERR_Wr = handles[1];
+
+            WinBase.STARTUPINFO startupInfo = new WinBase.STARTUPINFO();
+            startupInfo.hStdInput = hChildStd_IN_Rd;
+            startupInfo.hStdOutput = hChildStd_OUT_Wr;
+            startupInfo.hStdError = hChildStd_ERR_Wr;
+            startupInfo.dwFlags |= WinBase.STARTF_USESTDHANDLES;
+            WinBase.PROCESS_INFORMATION.ByReference processInformation = new WinBase.PROCESS_INFORMATION.ByReference();
+
+            boolean status = Kernel32.INSTANCE.CreateProcessW(null,
+                    daCmd.toCharArray(),
+                    null,
+                    null,
+                    true,
+                    new WinDef.DWORD(0),
+                    Pointer.NULL, //TODO: should be the environment
+                    workingDir.getAbsolutePath(),
+                    startupInfo,
+                    processInformation);
+            if (!status) {
+                throw new RuntimeException("Error executing CreateProcessW");
+            }
+
+            if (!Kernel32.INSTANCE.CloseHandle(hChildStd_IN_Rd)) throw new RuntimeException("Error CloseHandle IN_Rd");
+            if (!Kernel32.INSTANCE.CloseHandle(hChildStd_OUT_Wr)) throw new RuntimeException("Error CloseHandle OUT_Wr");
+            if (!Kernel32.INSTANCE.CloseHandle(hChildStd_ERR_Wr)) throw new RuntimeException("Error CloseHandle ERR_Wr");
+
+            this.processIs = new WinInputStream(hChildStd_OUT_Rd);
+            this.processOs = new WinOutputStream(hChildStd_IN_Wr);
+            this.processEs = new WinInputStream(hChildStd_ERR_Rd);
+            this.processInfo = processInformation;
+        } else {
+            this.process = Runtime.getRuntime().exec(command, makeEnv(env), workingDir);
+            this.processIs = process.getInputStream();
+            this.processOs = process.getOutputStream();
+            this.processEs = process.getErrorStream();
+        }
+        this.running = true;
+    }
+
+    private WinNT.HANDLE[] createPipe(boolean out) {
+        WinBase.SECURITY_ATTRIBUTES securityAttributes = new WinBase.SECURITY_ATTRIBUTES();
+        securityAttributes.bInheritHandle = true;
+
+        WinNT.HANDLEByReference hReadPipe = new WinNT.HANDLEByReference();
+        WinNT.HANDLEByReference hWritePipe = new WinNT.HANDLEByReference();
+
+        if (!Kernel32.INSTANCE.CreatePipe(hReadPipe, hWritePipe, securityAttributes, 0)) {
+            throw new RuntimeException("Error executing CreatePipe");
+        }
+
+        WinNT.HANDLE handleToMarkAsNotInherited = out ? hReadPipe.getValue() : hWritePipe.getValue();
+        if (!Kernel32.INSTANCE.SetHandleInformation(handleToMarkAsNotInherited, WinBase.HANDLE_FLAG_INHERIT, 0)) {
+            throw new RuntimeException("Error executing SetHandleInformation");
+        }
+
+        return new WinNT.HANDLE[]{hReadPipe.getValue(), hWritePipe.getValue()};
+    }
+
+    static class WinOutputStream extends OutputStream {
+        private final WinNT.HANDLE handle;
+
+        WinOutputStream(WinNT.HANDLE handle) {
+            this.handle = handle;
+        }
+
+        public void write(int b) throws IOException {
+            byte[] buffer = new byte[1];
+            buffer[0] = (byte) b;
+            IntByReference bytesWritten = new IntByReference();
+            if (!Kernel32.INSTANCE.WriteFile(handle, buffer, buffer.length, bytesWritten, null)) {
+                int err = Kernel32.INSTANCE.GetLastError();
+                throw new IOException("Error executing WriteFile (WinError=" + err + ")");
+            }
+        }
+    }
+
+    static class WinInputStream extends InputStream {
+        private final WinNT.HANDLE handle;
+
+        WinInputStream(WinNT.HANDLE handle) {
+            this.handle = handle;
+        }
+
+        public int read() throws IOException {
+            byte[] buffer = new byte[1];
+            IntByReference bytesRead = new IntByReference();
+            if (!Kernel32.INSTANCE.ReadFile(handle, buffer, buffer.length, bytesRead, null)) {
+                int err = Kernel32.INSTANCE.GetLastError();
+                if (err == WinError.ERROR_BROKEN_PIPE) {
+                    return -1;
+                }
+                throw new IOException("Error executing ReadFile: " + err);
+            }
+            if (bytesRead.getValue() == 0) {
+                return -1;
+            }
+            return buffer[0];
+        }
+    }
+
+    private boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("win");
+    }
 
   private Map<String, String> makeEnvMap(List<String> list) {
     Map<String, String> rv = new HashMap(System.getenv());
@@ -248,7 +381,7 @@ public class LinkedJavaProcess extends Process {
   @Override
   public synchronized InputStream getInputStream() {
     if (!this.running) throw new IllegalStateException("This LinkedJavaProcess is not yet running.");
-    return this.process.getInputStream();
+    return this.processIs;
   }
 
   public synchronized String[] getCommand() {
@@ -296,7 +429,7 @@ public class LinkedJavaProcess extends Process {
   @Override
   public synchronized InputStream getErrorStream() {
     if (!this.running) throw new IllegalStateException("This LinkedJavaProcess is not yet running.");
-    return this.process.getErrorStream();
+    return this.processEs;
   }
 
   /**
@@ -305,7 +438,7 @@ public class LinkedJavaProcess extends Process {
   @Override
   public synchronized OutputStream getOutputStream() {
     if (!this.running) throw new IllegalStateException("This LinkedJavaProcess is not yet running.");
-    return this.process.getOutputStream();
+    return this.processOs;
   }
 
   @Override
@@ -321,13 +454,29 @@ public class LinkedJavaProcess extends Process {
   @Override
   public int waitFor() throws InterruptedException {
     Process theProcess = null;
+    WinBase.PROCESS_INFORMATION.ByReference processInfo = null;
 
-    synchronized (this) {
-      if (!this.running) throw new IllegalStateException("This LinkedJavaProcess is not running.");
-      theProcess = this.process;
-    }
+        synchronized (this) {
+            if (!this.running) throw new IllegalStateException("This LinkedJavaProcess is not running.");
+            theProcess = this.process;
+            processInfo = this.processInfo;
+        }
 
-    int exitCode = theProcess.waitFor();
+        int exitCode;
+        if (isWindows()) {
+            Kernel32.INSTANCE.WaitForSingleObject(processInfo.hProcess, WinBase.INFINITE);
+            IntByReference rc = new IntByReference();
+            Kernel32.INSTANCE.GetExitCodeProcess(processInfo.hProcess, rc);
+            exitCode = rc.getValue();
+
+            Kernel32.INSTANCE.CloseHandle(processInfo.hProcess);
+
+            Kernel32.INSTANCE.CloseHandle(((WinInputStream) processIs).handle);
+            Kernel32.INSTANCE.CloseHandle(((WinInputStream) processEs).handle);
+            Kernel32.INSTANCE.CloseHandle(((WinOutputStream) processOs).handle);
+        } else {
+            exitCode = theProcess.waitFor();
+        }
 
     for (Iterator<StreamCopier> i = copiers.iterator(); i.hasNext();) {
       Thread t = i.next();
